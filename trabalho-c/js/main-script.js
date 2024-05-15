9118c12e28719c5a47de3298b1a362a5e202dfde
Henrique Soares <henrique.c.soares@tecnico.ulisboa.pt>
2024-05-15 20:42:29 +0100

    started work C.

---
+++ trabalho-c/js/main-script.js
@@ -0,0 +1,187 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
+import * as Stats from 'three/addons/libs/stats.module.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
+//////////////////////
+/* GLOBAL VARIABLES */
+//////////////////////
+
+var renderer, scene;
+var perspectiveCamera, stereoCamera;
+
+/////////////////////
+/* CREATE SCENE(S) */
+/////////////////////
+function createScene(){
+    'use strict';
+
+    scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(10));
+}
+
+//////////////////////
+/* CREATE CAMERA(S) */
+//////////////////////
+function createPerspectiveCamera() {
+    'use strict';
+    // PerspectiveCamera(fov, aspect, near, far)
+    perspectiveCamera = new THREE.PerspectiveCamera(70,
+        window.innerWidth / window.innerHeight, 1, 1000);
+    perspectiveCamera.position.set(50, 50, 50);
+    perspectiveCamera.lookAt(scene.position);
+}
+
+function createStereoCamera() {
+    'use strict';
+    // StereoCamera()
+    stereoCamera = THREE.StereoCamera();
+}
+
+/////////////////////
+/* CREATE LIGHT(S) */
+/////////////////////
+
+////////////////////////
+/* CREATE OBJECT3D(S) */
+////////////////////////
+
+//////////////////////
+/* CHECK COLLISIONS */
+//////////////////////
+function checkCollisions(){
+    'use strict';
+
+}
+
+///////////////////////
+/* HANDLE COLLISIONS */
+///////////////////////
+function handleCollisions(){
+    'use strict';
+
+}
+
+////////////
+/* UPDATE */
+////////////
+function update(){
+    'use strict';
+
+}
+
+/////////////
+/* DISPLAY */
+/////////////
+function render() {
+    'use strict';
+    renderer.render(scene, perspectiveCamera);
+}
+
+////////////////////////////////
+/* INITIALIZE ANIMATION CYCLE */
+////////////////////////////////
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    /* Ponto 8
+    document.body.appendChild(VRButton.createButton(renderer));
+    renderer.xr.enabled = true;
+    renderer.setAnimationLoop( function () {
+        renderer.render(scene, camera);
+    } );
+    createStereoCamera();
+    */
+
+    createScene();
+    createPerspectiveCamera();
+
+    const controls = new OrbitControls(perspectiveCamera, renderer.domElement);
+    
+    render(); // pode-se tirar?
+
+    window.addEventListener("resize", onResize);
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+}
+
+/////////////////////
+/* ANIMATION CYCLE */
+/////////////////////
+function animate() {
+    'use strict';
+
+    update();
+    render();
+
+    requestAnimationFrame(animate);
+}
+
+////////////////////////////
+/* RESIZE WINDOW CALLBACK */
+////////////////////////////
+function onResize() { 
+    'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        currentCamera.aspect = window.innerWidth / window.innerHeight;
+        currentCamera.updateProjectionMatrix();
+    }
+}
+
+///////////////////////
+/* KEY DOWN CALLBACK */
+///////////////////////
+function onKeyDown(e) {
+    'use strict';
+
+    switch(e.keyCode) {
+        case 49: // 1
+            break;
+        case 50: // 2
+            break;
+        case 51: // 3
+            break;
+        case 68: // D/d
+            break;
+        case 80: // P/p
+            break;
+        case 83: // S/s
+            break;
+        case 81: // Q/q
+            break;
+        case 87: // W/w
+            break;
+        case 69: // E/e
+            break;
+        case 82: // R/r
+            break;
+        case 84: // T/t
+            break;
+    }
+}
+
+///////////////////////
+/* KEY UP CALLBACK */
+///////////////////////
+function onKeyUp(e){
+    'use strict';
+
+    switch(e.keyCode) {
+        case 49: // 1
+            break;
+        case 50: // 2
+            break;
+        case 51: // 3
+            break;
+    }
+}
+
+init();
+animate();
